f006e9fc4cb2e8b50b3dfbeacd95dc341468a8bf
Siddhik234 <106161586+Siddhik234@users.noreply.github.com>
2022-09-28 13:23:47 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Home } from './component/Home';
+
+
+import {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+} from 'chart.js';
+
+import React,{useState} from 'react';
+import { Tabs,Tab } from "@mui/material";
+import { Onlinesales } from './component/0nlinesales';
+import { Athomesales } from './component/Athomesales';
+
+Chart.register(
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+);
+
+
+
 
 function App() {
+  const[val,setVal]=useState("") 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      
+   <Tabs value={val} onChange={(e,value)=>setVal(value)}>
+                <Tab label="Revenue Share Online / offline" value="online"/>
+                <Tab label="Revenue at Home and Out of Home" value="Home"/>
+              
+            </Tabs>
+            {val==="online" && <Onlinesales/>}
+            {val==="Home" && <Athomesales/>} 
+            <div>
+    <Home/>
+   </div>
+            
+      
+   </React.Fragment>
   );
 }
 
